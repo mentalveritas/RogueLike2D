b5f587204f5e7c11444f7611deeede0935461068
mentalveritas <mentalveritas@gmail.com>
2016-08-05 15:42:07 +0900

    Add RandomMap Generate

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System; //for Serializable
+using System.Collections.Generic; //for list
+using Random = UnityEngine.Random; //Select Unity Engine Random 
+
+public class BoardManager : MonoBehaviour
+{
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+
+        public Count(int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+
+    }
+
+    //Game board size
+    public int columns = 8;
+    public int rows = 8;
+
+    //Control how many objects are there in level
+    public Count wallCount = new Count(5, 9);
+    public Count foodCount = new Count(1, 5);
+
+    //GameObject
+    public GameObject exit;
+    public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
+    public GameObject[] foodTiles;
+    public GameObject[] enemyTiels;
+    public GameObject[] outerWallTiles;
+
+    //for fold hierarchy objects
+    private Transform boardHolder;
+    //Chasing objects position
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    void InitialiseList()
+    {
+        gridPositions.Clear();
+
+        for(int x = 1; x< columns -1; x++)
+        {
+            for(int y = 1; y < rows -1; y++)
+            {
+                gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+    }
+
+    //Setup floorTiles and outerWallTitles
+    void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+
+        for(int x=-1; x<columns + 1; x++)
+        {
+            for(int y = -1; y <rows + 1; y++)
+            {
+                GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                if(x == -1 || x == columns || y == -1 || y == rows)
+                {
+                    //Make outerWall objects
+                    toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
+                }
+
+                //Make instance GameObject, Quarternion.identity => no rotate
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                //Set parent
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    //Make Random Position
+    Vector3 RandomPosition()
+    {
+        int randomIndex = Random.Range(0, gridPositions.Count);
+        Vector3 randomPosition = gridPositions[randomIndex];
+        //prevent duplication
+        gridPositions.RemoveAt(randomIndex);
+
+        return randomPosition;
+    }
+
+    //Spawn Layout Objects using randompositon
+    void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
+    {
+        //Count how many object in level
+        int objectCount = Random.Range(minimum, maximum + 1);
+
+        for(int i = 0; i < objectCount; i++)
+        {
+            Vector3 randomPosition = RandomPosition();
+            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+            Instantiate(tileChoice, randomPosition, Quaternion.identity);
+        }
+
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
