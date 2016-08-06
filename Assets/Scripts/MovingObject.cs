9d920ad156e21b622598f168f4077bd40fa136c4
mentalveritas <mentalveritas@gmail.com>
2016-08-06 21:35:20 +0900

    Adding Moving Object

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class MovingObject : MonoBehaviour {
+
+    public float moveTime = .1f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+
+	// Use this for initialization
+	protected virtual void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+	}
+
+    protected IEnumerator SmoothMovement(Vector3 end)
+    {
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        while(sqrRemainingDistance > float.Epsilon)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+            rb2D.MovePosition(newPosition);
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+            //Waiting next frame
+            yield return null;
+        }
+    }
+
+    protected abstract void OnCantMove<T>(T componet)
+        where T : Component;
+    
+
+    
+}
